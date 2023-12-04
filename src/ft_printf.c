02899a9d81274f5bced0aa1a1a37ed43861996c9
Juste_ozan <117074595+osahinGuardia@users.noreply.github.com>
2023-12-04 17:12:16 +0100

    explore th basics of printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -10,6 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "ft_printf.h"
+//#include "libft.h"
+//#include "ft_printf.h"
+#include <stdarg.h>
 
+void	find_format(char c)
+{
+
+	if (c == 'c')
+		ft_putchar();
+	else if (c == 's')
+		ft_putstr();
+	else if (c == 'p')
+		ft_print_ptr();
+	else if (c == 'd')
+		ft_putnbr()
+	else if (c == 'i')
+		ft_putnbr_base( , 10);
+	else if (c == 'u')
+		ft_putnbr();
+	else if (c == 'x')
+		ft_putnbr_base( , 16);
+	else if (c == 'X')
+		ft_putnbr_base( , 16);
+	else if (c == '%')
+		return (0);
+	else
+		return (0);
+}
+
+int	ft_printf(const char *entry, ...)
+{
+	int	i;
+	int	value;
+
+	i = -1;
+	value = 0;
+	while (entry[++i])
+	{
+		if (entry[i] == '%')
+		{
+			print_to_format(entry[i + 1]);
+		}
+	}
+	return (value);
+}
+
+#include <stdio.h>
+
+int main()
+{
+	char	*str = "nop";
+	int value = ft_printf("le poil %s au feu", str);
+	printf("%d\n", value);
+	return (0);
+}
